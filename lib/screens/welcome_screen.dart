306dab953d048aacd675c8a0ea93b27eabcacc30
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-11-04 19:04:23 +0500

    Custom Flutter Animations with the Animation Controller.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,16 +5,40 @@ import 'package:flutter/material.dart';
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
   @override
-
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
   @override
+  void initState() {
+    super.initState();
+
+    controller =
+        AnimationController(duration: Duration(seconds: 1), vsync: this);
+
+    animation = ColorTween(begin: Colors.blueGrey, end: Colors.white).animate(controller);
+
+    controller.forward();
+
+    controller.addListener(() {
+      setState(() {});
+      print(controller.value);
+    });
+  }
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: animation.value,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
