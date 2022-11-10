38c8f569c0f82de19fc3b9cad2aac645c84c5b42
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-11-10 20:40:38 +0500

    Registering Users with Firebase using FirebaseAuth.

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flash_chat/constants.dart';
 import 'package:flash_chat/rounded_button.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'chat_screen.dart';
 
 class RegistrationScreen extends StatefulWidget {
   static const id = 'registration_screen';
@@ -9,6 +11,10 @@ class RegistrationScreen extends StatefulWidget {
 }
 
 class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _auth = FirebaseAuth.instance;
+  String email;
+  String password;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,30 +36,48 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
               height: 48.0,
             ),
             TextField(
+              keyboardAppearance: Brightness.dark,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.emailAddress,
               onChanged: (value) {
-                //Do something with the user input.
+                email = value;
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
+              decoration:
+                  kTextFieldDecoration.copyWith(hintText: 'Enter Your Email'),
             ),
             SizedBox(
               height: 8.0,
             ),
             TextField(
+              keyboardAppearance: Brightness.dark,
+              textAlign: TextAlign.center,
+              obscureText: true,
               onChanged: (value) {
-                //Do something with the user input.
+                password = value;
               },
-              decoration: kTextFieldDecoration.copyWith(hintText: 'Enter Your Password'),
+              decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter Your Password'),
             ),
             SizedBox(
               height: 24.0,
             ),
-           RoundedButton(
-             title: 'Register',
-             color: Colors.blueAccent,
-             onPressed: (){
+            RoundedButton(
+              title: 'Register',
+              color: Colors.blueAccent,
+              onPressed: () async {
 
-             },
-           )
+                try {
+                  final newUser = await _auth.createUserWithEmailAndPassword(
+                      email: email, password: password);
+                  if (newUser != null) {
+                    Navigator.pushNamed(context, ChatScreen.id);
+                  }
+
+                } catch (e) {
+                  print(e);
+                }
+              },
+            )
           ],
         ),
       ),
