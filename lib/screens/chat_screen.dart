a6692c875833bcd54e7a8e2d18a4e4b3165f8b70
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-11-11 17:28:11 +0500

    Saving Data into Cloud Firestore.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,7 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatefulWidget {
   static const id = 'chat_screen';
@@ -10,9 +10,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-
   final _auth = FirebaseAuth.instance;
-
+  final _firestore = FirebaseFirestore.instance;
+  String messageText;
   User loggedInUser;
 
   @override
@@ -46,6 +46,7 @@ class _ChatScreenState extends State<ChatScreen> {
               onPressed: () {
                 _auth.signOut();
                 Navigator.pop(context);
+                print('user logout successfully');
               }),
         ],
         title: Text('⚡️Chat'),
@@ -64,14 +65,17 @@ class _ChatScreenState extends State<ChatScreen> {
                   Expanded(
                     child: TextField(
                       onChanged: (value) {
-                        //Do something with the user input.
+                        messageText = value;
                       },
                       decoration: kMessageTextFieldDecoration,
                     ),
                   ),
                   FlatButton(
                     onPressed: () {
-                      //Implement send functionality.
+                      _firestore.collection('messages').add({
+                        'text': messageText,
+                        'sender': loggedInUser.email,
+                      });
                     },
                     child: Text(
                       'Send',
